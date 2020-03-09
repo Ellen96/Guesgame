c714350e6e885f1f782b0b609a99c2edd1e35b6f
Ellen96 <61159491+Ellen96@users.noreply.github.com>
2020-03-09 19:40:49 +0100

    eigen code ingevoegd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Guesgame';
+  afteller: { min: number, sec: number };
+  afwijking: number;
+  aantalGokken: number;
+  teGokkenGetal: number;
+  gok: number;
+  spelGewonnen:boolean;
+  spelVerloren:boolean;
+  
+  constructor() {
+    this.restartSpel();
+  }
+  restartSpel() {
+    this.aantalGokken = 0;
+    this.teGokkenGetal = Math.floor(Math.random() * 100 + 1);
+    this.gok = null;
+    this.afwijking = null;
+    this.spelGewonnen = false;
+    this.spelVerloren = false;
+    this.afteller;
+    this.startTimer();
+  }
+  controleGok() {
+    this.afwijking = this.teGokkenGetal - this.gok;
+    this.aantalGokken = this.aantalGokken + 1;
+    if(this.afwijking===0)
+    {this.spelGewonnen=true;   
+    }
+    if (this.aantalGokken==10) {
+      this.spelVerloren=true;
+    }
+  }
+  startTimer() {
+    this.afteller = { min: 5, sec: 0 }
+    let intervalId = setInterval(() => {
+      if (this.afteller.sec - 1 == -1) {
+        this.afteller.min -= 1;
+        this.afteller.sec = 59
+      } 
+      else this.afteller.sec -= 1
+      if (this.afteller.min === 0 && this.afteller.sec == 0) 
+      {clearInterval(intervalId)
+      this.spelVerloren=true;
+      }
+    }, 1000)
+  }
+  
 }
